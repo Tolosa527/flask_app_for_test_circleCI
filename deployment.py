e01bdf9f9c6f4c6e3fe04426b1ef871d3e114a13
Tolosa527 <matiaszulberti@gmail.com>
2022-07-29 13:06:33 +0200

    Add somethings to the script

--- deployment.py
+++ deployment.py
@@ -2,6 +2,7 @@ import sys
 import subprocess
 
 # implement pip as a subprocess:
+subprocess.check_call(['git', 'checkout', 'main'])
 subprocess.check_call([sys.executable, '-m', 'pip', 'install','-r','requirements.txt'])
 subprocess.check_call(['pytest', 'tests/'])
 subprocess.check_call(['python3', 'script.py'])
--- script.py
+++ script.py
@@ -2,5 +2,5 @@ from datetime import datetime
 
 if __name__ == "__main__":
     print(f"\nINFO: This script was ran at {datetime.now()}\n")
-    with open("aux.txt", "w") as f:
+    with open("../aux.txt", "w") as f:
         f.write(f"This script was ran at {datetime.now()}")
