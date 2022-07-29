e3b7158d37ca2dcd6eaffcb8e94b258676e4cd3c
Tolosa527 <matiaszulberti@gmail.com>
2022-07-29 13:09:50 +0200

    deployment.py changed

--- deployment.py
+++ deployment.py
@@ -3,6 +3,7 @@ import subprocess
 
 # implement pip as a subprocess:
 subprocess.check_call(['git', 'checkout', 'main'])
+subprocess.check_call(['git', 'pull'])
 subprocess.check_call([sys.executable, '-m', 'pip', 'install','-r','requirements.txt'])
 subprocess.check_call(['pytest', 'tests/'])
 subprocess.check_call(['python3', 'script.py'])
