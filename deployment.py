4b37963f5392e3c3d706a898fc46138ff0c1901e
Tolosa527 <matiaszulberti@gmail.com>
2022-07-29 12:50:13 +0200

    Add a script to automate the deployment in EC2

---
+++ deployment.py
@@ -0,0 +1,7 @@
+import sys
+import subprocess
+
+# implement pip as a subprocess:
+subprocess.check_call([sys.executable, '-m', 'pip', 'install','-r','requirements.txt'])
+subprocess.check_call(['pytest', 'tests/'])
+subprocess.check_call(['python3', 'script.py'])
--- script.py
+++ script.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 
 if __name__ == "__main__":
+    print(f"\nINFO: This script was ran at {datetime.now()}\n")
     with open("aux.txt", "w") as f:
         f.write(f"This script was ran at {datetime.now()}")
