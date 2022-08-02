3cb3c3527baaf615d04d4d22cd99b0e87dc9d8b7
Tolosa527 <matiaszulberti@gmail.com>
2022-08-02 09:46:44 +0200

    Add two characters

--- script.py
+++ script.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 
 if __name__ == "__main__":
-    print(f"\nINFO: This script was ran at {datetime.now()}\n")
+    print(f"\n## INFO: This script was ran at {datetime.now()}\n")
     with open("../aux.txt", "w") as f:
         f.write(f"This script was ran at {datetime.now()}")
